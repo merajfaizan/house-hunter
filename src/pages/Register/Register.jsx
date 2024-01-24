28ba8dc6c6c1e0f17ee05b4f5e894f6faad786bf
Meraj Faizan <mdmaraj1500@gmail.com>
2024-01-24 09:01:49 +0600

    project setup created

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,9 @@
+const Register = () => {
+  return (
+    <div>
+      <h1>this Register page</h1>
+    </div>
+  );
+};
+
+export default Register;
---
+++ src/router/routes.jsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+import Main from "../layout/Main";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
+
+export default router;
